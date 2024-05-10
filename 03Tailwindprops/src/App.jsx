363aa3bbf55afebb4a1184443002a4a82970e28e
sipunpanda <sipunpanda26@gmail.com>
2024-05-10 22:22:59 +0530

    props in react

---
+++ 03Tailwindprops/src/App.jsx
@@ -0,0 +1,30 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  
+  let obj = {
+    username: "sipun",
+    age:22
+  }
+
+  let arr = [1,2,3]
+
+  return (
+    <>
+     <h1 className='bg-green-500 text-black p-4 rounded-xl mb-5'>
+      Tailwind Test
+     </h1>
+
+     <Card username='sipun' someobj = {obj} arr = {arr} btnTxt = "Visit Me"/>
+     <Card  />
+
+
+    </>
+  )
+}
+
+export default App
