0a54682ee6b8ee38c33ac1e03c473f05772262ee
sipunpanda <sipunpanda26@gmail.com>
2024-11-07 17:55:14 +0530

    reducer in reduxToolkit

---
+++ 11reduxtoolkittodo/src/feature/todo/todo.Slice.js
@@ -0,0 +1,27 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{
+        id: 1,
+        text: "Hello World!"
+    }]
+}
+
+function sayHello(state, action) {
+    const todo = {
+        id: nanoid(),
+        text: action.payload
+    }
+    state.todos.push(todo);
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: sayHello,
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo)=> todo.id !== action.payload.id) 
+         },
+    }
+})
