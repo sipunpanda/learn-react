8604ddfe47dfce6bcfeb01d075b18c144033399b
sipunpanda <sipunpanda26@gmail.com>
2024-11-11 22:12:00 +0530

    Redux ToolKit

--- 11reduxtoolkittodo/src/App.jsx
+++ 11reduxtoolkittodo/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
     <>
-<h1>App</h1>
+<h1 className='text-9xl text-gray-200'>App</h1>
+<AddTodo/>
+<Todos/>
     </>
   )
 }
--- 11reduxtoolkittodo/src/app/store.js
+++ 11reduxtoolkittodo/src/app/store.js
@@ -1,5 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
+import todoReducer from '../feature/todo/todo.Slice'
+
 
 export const store = configureStore({
-    
+    reducer: todoReducer
 })
