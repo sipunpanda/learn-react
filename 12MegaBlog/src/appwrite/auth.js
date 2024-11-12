d684c978940b252bb1c9bfdc9e0b598a2a7fccb3
sipunpanda <sipunpanda26@gmail.com>
2024-11-13 00:33:26 +0530

    auth setup

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,14 +1,64 @@
 import conf from "../conf/conf";
 
-import {Client, Account, Id} from 'appwrite'
+import { Client, Account, ID } from 'appwrite'
 
 export class AuthService {
+    client = new Client();
+    account ;
+
     constructor() {
         this.client
-            .setEndPoint(conf.appwriteUrl)
+            .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
-            this.account = new Account(this.client)
+        this.account = new Account(this.client)
     }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.login({email, password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    }
+
+async login(email, password){
+    try {
+        await this.account.createEmailPasswordSession(email, password)
+        
+    } catch (error) {
+        throw error
+    }
+}
+
+
+async getCurrentUser(){
+    try {
+       return await this.account.get()
+    } catch (error) {
+        throw error        
+    }
+    return null;
+}
+
+
+async logout(){
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+        throw error
+        }
+        
+}
+
+
+
+
+
 }
 
 const authService = new AuthService();
